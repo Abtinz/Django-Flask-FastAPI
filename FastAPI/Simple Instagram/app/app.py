7aacd72bb5f89493ea15b4df0d0034f571af5bcc
Abtinz <Abtinzandi@gmail.com>
2026-01-20 19:22:22 -0500

    defining routers

--- FastAPI/Simple Instagram/app/app.py
+++ FastAPI/Simple Instagram/app/app.py
@@ -1,10 +1,12 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, UploadFile, File, Form, Depends
+from fastapi import 
 from app.db import create_db_and_tables, Post, get_async_session
 from sqlalchemy import text, select
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.schemas import PostResponse
 from app.imagekit_manager import ImageKitHandler
+from app.users_manager import fastapi_users, current_active_user, auth_backend
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,6 +15,16 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
+)
+app.include_router(
+    fastapi_users.get_register_router(), prefix="/auth", tags=["auth"]
+)
+app.include_router(
+    fastapi_users.get_users_router(), prefix="/users", tags=["users"]
+)       
 
 @app.post("/upload")
 async def upload_file(
@@ -43,3 +55,17 @@ async def get_posts(session: AsyncSession = Depends(get_async_session)):
     result = await session.execute(select(Post))
     posts = result.scalars().all()
     return posts
+
+@app.delete("/posts/{post_id}")
+async def delete_post(post_id: str, session: AsyncSession = Depends(get_async_session)):
+    result = await session.execute(select(Post).where(Post.id == post_id))
+    post = result.scalar_one_or_none()
+    if post is None:
+        return {
+            "error": "Post not found",
+            "code": HttpStatus.NOT_FOUND
+            }
+
+    await session.delete(post)
+    await session.commit()
+    return {"message": "Post deleted successfully"}
