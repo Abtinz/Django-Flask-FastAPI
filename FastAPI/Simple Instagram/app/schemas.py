835e0969a79b6207dec147fc2a030b625772d282
Abtinz <Abtinzandi@gmail.com>
2026-01-20 19:28:20 -0500

    user's needed schemas

--- FastAPI/Simple Instagram/app/schemas.py
+++ FastAPI/Simple Instagram/app/schemas.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from datetime import datetime
 from uuid import UUID
+from fastapi_users import schemas
 
 class PostCreate(BaseModel):
     '''Schema for creating a new post'''
@@ -15,6 +16,31 @@ class PostResponse(BaseModel):
     file_type: str
     file_name: str
     created_at: datetime
+    user_id: UUID
+    user: UserResponse
+
+    class Config:
+        from_attributes = True
+
+class UserRead(schemas.BaseUser[UUID]):
+    '''Schema for reading user information'''
+    pass
+
+class UserCreate(schemas.BaseUserCreate):
+    '''Schema for creating a new user'''
+    pass    
+
+class UserUpdate(schemas.BaseUserUpdate):
+    '''Schema for updating user information'''
+    pass 
+
+class UserResponse(BaseModel):
+    '''Schema for returning user information'''
+    id: UUID
+    email: str
+    is_active: bool
+    is_superuser: bool
+    is_verified: bool
 
     class Config:
         from_attributes = True
